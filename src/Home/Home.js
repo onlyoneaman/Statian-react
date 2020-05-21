f84e0efb14485243135a3edab3dd165bd80f4c29
kumar.97 <kumar.97@iitj.ac.in>
2020-05-21 17:31:49 +0530

    Added Google analytics

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,8 +1,17 @@
 import React from "react";
 import Layout from "../components/Layout";
+import ReactGA from 'react-ga'
 
 
 class Home extends React.Component{
+    componentDidMount() {
+        ReactGA.initialize('UA-141356997-7', {
+            debug: false,
+            titleCase: false,
+        });
+        ReactGA.pageview(window.location.pathname + window.location.search);
+    }
+
     render() {
         return(
             <div>
